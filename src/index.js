be15ab7d515d6321b62a62cb590ef4305c59463d
shabbirvaghela <69663132+shabbirvaghela@users.noreply.github.com>
2023-06-23 22:32:41 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AppProvider } from "./context";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<AppProvider>
+			<App />
+		</AppProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
